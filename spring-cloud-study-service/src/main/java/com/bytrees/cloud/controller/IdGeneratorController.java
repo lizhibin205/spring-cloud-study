1b41ced87a30deb7d15a09cd0f95301be6af9c09
lizhibin205 <331313869@qq.com>
2020-08-30 23:03:20 +0800

    ID生成器接入微服务接口

--- spring-cloud-study-service/src/main/java/com/bytrees/cloud/controller/IdGeneratorController.java
+++ spring-cloud-study-service/src/main/java/com/bytrees/cloud/controller/IdGeneratorController.java
@@ -1,5 +1,7 @@
 package com.bytrees.cloud.controller;
 
+import com.bytrees.cloud.id.generator.Generator;
+import com.bytrees.cloud.id.generator.creator.DefaultTimeCreator;
 import com.bytrees.cloud.response.BaseResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,13 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/idGenerator")
 public class IdGeneratorController {
+    private static final Generator generator = Generator.getInstance(new DefaultTimeCreator(), 1);
+
+    @GetMapping("/getId")
+    public BaseResponse<Long> getId() {
+        return BaseResponse.success(generator.genId(1));
+    }
+
     @GetMapping("/getUUID")
     public BaseResponse<String> getUUID() {
         return BaseResponse.success(UUID.randomUUID().toString());
