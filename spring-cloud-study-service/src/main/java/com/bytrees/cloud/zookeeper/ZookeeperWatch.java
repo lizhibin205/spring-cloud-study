e4cd4f3306ebfe818ac7aad21e2947233d809294
lizhibin205 <331313869@qq.com>
2020-09-13 01:14:11 +0800

    groovy log

--- spring-cloud-study-service/src/main/java/com/bytrees/cloud/zookeeper/ZookeeperWatch.java
+++ spring-cloud-study-service/src/main/java/com/bytrees/cloud/zookeeper/ZookeeperWatch.java
@@ -2,8 +2,14 @@ package com.bytrees.cloud.zookeeper;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ZookeeperWatch implements Watcher {
+    private static final Logger logger = LoggerFactory.getLogger(ZookeeperWatch.class);
+
     @Override
-    public void process(WatchedEvent event) {}
+    public void process(WatchedEvent event) {
+        logger.info("ZookeeperWatch process: {}", event);
+    }
 }
--- spring-cloud-study-service/src/main/resources/logback.groovy
+++ spring-cloud-study-service/src/main/resources/logback.groovy
@@ -1,27 +1,28 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.rolling.RollingFileAppender
-import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy
-import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+import ch.qos.logback.classic.AsyncAppender
 
-context.name = "eureka-server"
-appender("CONSOLE", ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "[%d{yyyy-MM-dd HH:mm:ss.SSS}] [%level] [%thread] [%logger{50}] >>> %msg%n"
-    }
-}
-appender("FILE", RollingFileAppender) {
-    file = "/apps/logs/log_receiver/spring-cloud-study/application.log"
-    append = true
+def defaultLogLevel = INFO
+def appenderList = []
+
+context.name = "eureka-study"
+
+appender("ROLLING", RollingFileAppender) {
+    file = "/apps/logs/log_receiver/eureka-server/application.log"
     encoder(PatternLayoutEncoder) {
         pattern = "[%d{yyyy-MM-dd HH:mm:ss.SSS}] [%level] [%thread] [%logger{50}] >>> %msg%n"
     }
     rollingPolicy(TimeBasedRollingPolicy) {
-        fileNamePattern = "data.%d{yyyy-MM-dd}.log"
+        fileNamePattern = "/apps/logs/log_receiver/eureka-server/application.%d{yyyy-MM-dd}.log"
         maxHistory = 7
-        totalSizeCap = "1GB"
-    }
-    triggeringPolicy(SizeBasedTriggeringPolicy) {
-        maxFileSize = "5MB"
     }
 }
+appender("ROLLING-ASYNC", AsyncAppender) {
+    discardingThreshold = 2
+    queueSize = 1024
+    neverBlock = true
+    appenderRef("ROLLING")
+}
+
+appenderList.add("ROLLING-ASYNC");
+
+logger("com.bytrees", defaultLogLevel, appenderList, false)
